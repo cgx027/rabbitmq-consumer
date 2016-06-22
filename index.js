0610ffa68196f14e27e1e3ef51c3f0d69ecc3dbd
Ted Chen <ted.chen@emc.com>
2016-06-22 13:10:07 +0000

    test

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+'use strict';
+
 var amqp = require('amqp');
 
 var exchange_name = 'on.events';
-var routing_key = 'node.alert.*';
+var routing_key_name = 'node.alert.*';
 var queue_name = exchange_name + '-' + routing_key + '-test.consumer';
+var url = "amqp://localhost";
 var options = {
-    url: "amqp://localhost"
+    url: url
 };
 var clientOptions = {
     reconnect: true,
@@ -12,6 +15,13 @@ var clientOptions = {
     reconnectExponentialLimit: 120000,
     reconnectBackoffTime: 1000
 };
+
+var args = process.argv.slice(2);
+
+var exchange = args[0] || exchange_name;
+var routing_key = args[1] || routing_key_name;
+
+console.log('monitoring exchange: ' + exchange + ' routing key: ' + routing_key);
 
 var connection = amqp.createConnection(options, clientOptions);
 
@@ -25,13 +35,13 @@ connection.on('ready', function () {
     // Use the default 'amq.topic' exchange
     connection.queue(queue_name, function (q) {
         // Catch all messages
-        q.bind(exchange_name, routing_key);
+        q.bind(exchange, routing_key);
 
         // Receive messages
         q.subscribe(function (message) {
             // Print messages to stdout
             console.log(message);
-            console.log(String(message.data));
+            console.log('message data: ', String(message.data));
         });
     });
 });
